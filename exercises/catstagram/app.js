e59310c256b146c129c87a3db7bc56c9a07c1a91
Aysegul Yonet <aysegulyonet@gmail.com>
2015-01-10 14:18:39 -0800

    Exercise added explanation

--- exercises/catstagram/app.js
+++ exercises/catstagram/app.js
@@ -1,18 +1,16 @@
 var app = angular.module('catstagram', []);
 app.controller('CatController', ['$scope', '$http', function($scope, $http){
 
+	//Get the cats data using $http from data.json and assign the data to $scope.cats variable
 	$http.get('data.json')
 		.success(function(data, status, headers, config) {
 			$scope.cats = data.cats;
 			console.log('$scope.cats', $scope.cats)
 		});
-	$scope.like = function(index) {
-		$scope.cats[index].likes ++;
-	}
 
-	$scope.addCat = function(newCat) {
-		newCat.likes = 1;
-		$scope.cats.unshift(newCat);
-		$scope.newCat = {};
-	}
+	//Write a $scope.like function that takes index as an argument and adds 1 to likes attribute
+	$scope.like = 
+
+	//Add newCat to the beginning of the $scope.cats array
+	$scope.addCat = 
 }]);
